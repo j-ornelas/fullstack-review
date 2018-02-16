2e364fcb44751ecb7c762b6d20094ff38439e25d
j-ornelas <johnnyoh1988@gmail.com>
2018-02-16 10:53:40 -0800

    Set up schema

--- database/index.js
+++ database/index.js
@@ -3,6 +3,27 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+    "id": Number,
+    "name": String,
+    "full_name": String,
+    "owner": {
+      "login": String,
+      "id": Number,
+      "avatar_url": String,
+      "gravatar_id": String,
+      "url": String,
+      "html_url": String,
+      "followers_url": String,
+      "following_url": String,
+      "gists_url": String,
+      "starred_url": String,
+      "subscriptions_url": String,
+      "organizations_url": String,
+      "repos_url": String,
+      "events_url": String,
+      "received_events_url": String,
+      "type": String,
+      "site_admin": Boolean
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
